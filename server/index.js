da08517b9f0e0cdd9d4de70069630edc70fa625c
Gagan <vishwakarmagagan24@gmail.com>
2023-08-18 18:11:29 +0530

    .
    .

--- server/index.js
+++ server/index.js
@@ -2,14 +2,12 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const app=express();
-const port =3006
-app.use(bodyParser);
-app.use(cors) 
+const app = express();
+const port = 3006
+app.use(bodyParser.json({ limit: "30mb",extended:true }));
+app.use(cors)
 
-app.get("/",(req,res)=>{
-      res.send("hello world")
+app.get("/", (req, res) => {
+    res.send("hello world")
 })
-app.listen(port ,(err)=>{
-    console.log(err ,"live at port " + port)
-})
+app.listen(port)
